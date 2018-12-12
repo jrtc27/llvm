27dbf18c884fec91aa34deb8a9bb9bc53f25b386
James Clarke <jrtc27@jrtc27.com>
2018-12-12 21:27:29 +0000

    [RISCV] Omit explicit 0 offset in PrintAsmMemoryOperand
    This allows memory operands to also be printed for RVA instructions
    which take no offset when using inline assembly with the A constraint.

--- lib/Target/RISCV/RISCVAsmPrinter.cpp
+++ lib/Target/RISCV/RISCVAsmPrinter.cpp
@@ -141,7 +141,7 @@ bool RISCVAsmPrinter::PrintAsmMemoryOperand(const MachineInstr *MI,
     if (!MO.isReg())
       return true;
 
-    OS << "0(" << RISCVInstPrinter::getRegisterName(MO.getReg()) << ")";
+    OS << "(" << RISCVInstPrinter::getRegisterName(MO.getReg()) << ")";
     return false;
   }
 
